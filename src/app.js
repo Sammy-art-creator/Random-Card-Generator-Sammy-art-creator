3d2bc3ad16363f324aff917018180a0fd55c6016
Sammy-art-creator <svmoralesz@gmail.com>
2025-01-24 23:27:03 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomsymbol = symbol[Math.floor(Math.random() * symbol.length)];
+  const randomvalues = values[Math.floor(Math.random() * values.length)];
+  if (randomsymbol === "♥" || randomsymbol === "♦") {
+    let cardsred = document.getElementById("card");
+    cardsred.style.color = "red";
+  } else {
+    let cardsblack = document.getElementById("card");
+    cardsblack.style.color = "black";
+  }
+
+  document.getElementById("symbol-top").innerText = randomsymbol;
+  document.getElementById("value").innerText = randomvalues;
+  document.getElementById("symbol-bottom").innerText = randomsymbol;
 };
